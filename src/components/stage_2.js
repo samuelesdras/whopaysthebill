1bd7d4f603a4f25531ddaa3b04cd7c709fdb740a
Samuel Esdras Pereira Barbosa <samuelesdras@yahoo.com.br>
2021-01-31 20:46:55 -0300

    it works!

--- src/components/stage_2.js
+++ src/components/stage_2.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useContext} from 'react';
+
+import { MyContext} from '../context';
 
 const Stage2 = () => {
+  const context = useContext(MyContext);
+
   return (
-    <div className="App">
-      Stage 2
-    </div>
+    <>
+      <div className="result_wrapper">
+        <h3>The looser is</h3>
+        <div>{context.state.result}</div>
+      </div>
+
+      <div
+        className="action_button"  
+      >
+        Start Over!
+      </div>
+      <div
+        className="action_button btn_2"  
+        onClick={() => context.getNewLooser()}
+      >
+        Get new looser!!
+      </div>
+    </>
   );
 }
 
